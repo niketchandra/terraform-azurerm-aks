112a42d7f3267d26b6e7c5c8fd3a75fabd168f35
Niket Chandrawanshi <niket.chandrawanshi@outlook.com>
2022-04-24 22:50:30 +0530

    update 2.0

--- aks.tf
+++ aks.tf
@@ -1,9 +1,9 @@
-#module "resourcegroup1" {
-#  source            = "./Modules/AKS"
-#  akcname           = var.aksname
-#  resourcegroupname = var.rgname
-#  location          = var.location
-#  dnsprefix         = var.dnsprefix
-#  vmsize            = var.sku
-#  environment       = var.env
-#}
+module "resourcegroup1" {
+  source            = "./Modules/AKS"
+  akcname           = var.aksname
+  resourcegroupname = var.rgname
+  location          = var.location
+  dnsprefix         = var.dnsprefix
+  vmsize            = var.sku
+  environment       = var.env
+}
